1f5e5c95375f39705bcb049c33a53e1b8a1a6db1
liyanxiang <270489077@qq.com>
2017-06-29 15:25:02 +0800

    change jade to html!

--- static/routes/index.js
+++ static/routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     console.log("接收到请求");
-  res.render('../public/gakki');
+  res.sendFile(path.resolve(__dirname, '..') + '/public/gakki.html');
 });
 
 module.exports = router;
